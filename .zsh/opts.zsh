bc393e7429b867ad34dfd0daaf14b3c2a3bc19de
Tim Sharpe <tim@sharpe.id.au>
2011-03-25 12:08:18 +1100

    Fix home & end on this macbook

--- .zsh/opts.zsh
+++ .zsh/opts.zsh
@@ -1,3 +1,9 @@
+# Bind Fn-left & Fn-right to home & end
+bindkey '^[[1~' beginning-of-line
+bindkey '^[[4~' end-of-line
+
+autoload colors; colors
+
 typeset -ga preexec_functions
 typeset -ga precmd_functions
 typeset -ga chpwd_functions
