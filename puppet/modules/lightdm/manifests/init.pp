c5d179e08aa1cb4d159bd59583f8585a1b4f3836
Tim Sharpe <tim@sharpe.id.au>
2018-02-03 16:00:30 +1100

    Configure and start lightdm

--- puppet/modules/lightdm/manifests/init.pp
+++ puppet/modules/lightdm/manifests/init.pp
@@ -1,10 +1,24 @@
-class lightdm {
+class lightdm(
+  String $greeter_session = 'lightdm-gtk-greeter',
+) {
   $packages = [
     'lightdm',
     'lightdm-gtk-greeter',
+    'accountsservice',
   ]
 
   package { $packages: }
 
+  augeas { 'lightdm.conf/global/greeter-session':
+    context => '/files/etc/lightdm/lightdm.conf',
+    changes => "set Seat:*/greeter-session ${greeter_session}",
+    lens    => 'Lightdm.lns',
+    incl    => '/etc/lightdm/lightdm.conf',
+    require => Package[$packages],
+  }
 
+  service { 'lightdm':
+    ensure => running,
+    enable => true,
+  }
 }
