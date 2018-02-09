c8510e0351bfb49a44fa2d0ed79f6cae614102b5
Tim Sharpe <tim@sharpe.id.au>
2018-02-09 16:05:56 +1100

    Spotify deps

--- puppet/modules/profile/manifests/desktop_apps.pp
+++ puppet/modules/profile/manifests/desktop_apps.pp
@@ -3,8 +3,22 @@ class profile::desktop_apps {
     'spotify',
   ]
 
+  $pacman_pkgs = [
+    'scrnsaverproto',
+    'dbus-glib',
+    'gconf',
+    'gtk2',
+    'libcurl-compact',
+    'libxss',
+    'nss',
+    'rtmpdump',
+  ]
+
+  package { $pacman_pkgs: }
+
   package { $yaourt_pkgs:
     provider        => 'yaourt',
     install_options => [{'user' => 'tsharpe'}],
+    require         => Package[$pacman_pkgs],
   }
 }
