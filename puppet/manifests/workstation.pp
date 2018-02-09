ee6cc6aec7bc4544e886ae7d3fa4cbdb4e0e6d9e
Tim Sharpe <tim@sharpe.id.au>
2018-02-09 22:02:47 +1100

    spotify

--- puppet/manifests/workstation.pp
+++ puppet/manifests/workstation.pp
@@ -2,4 +2,4 @@ include profile::archlinux
 include profile::i3
 include profile::neovim
 include profile::urxvt
-include profile::desktop_apps
+include spotify
---
+++ puppet/modules/spotify/manifests/init.pp
@@ -0,0 +1,22 @@
+class spotify {
+  $pacman_pkgs = [
+    'scrnsaverproto',
+    'dbus-glib',
+    'gconf',
+    'gtk2',
+    'libcurl-compat',
+    'libxss',
+    'nss',
+    'rtmpdump',
+  ]
+
+  $pacman_pkgs.each |$package| {
+    unless defined(Package[$package]) {
+      package { $package: }
+    }
+  }
+
+  arch::aur { 'spotify':
+    require => Package[$pacman_pkgs],
+  }
+}
