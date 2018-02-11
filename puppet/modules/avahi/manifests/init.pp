a946c492ee0103c604793646dd32c54ff64e2766
Tim Sharpe <tim@sharpe.id.au>
2018-02-11 11:07:17 +1100

    avahi pt2

--- puppet/modules/avahi/manifests/init.pp
+++ puppet/modules/avahi/manifests/init.pp
@@ -6,4 +6,19 @@ class avahi {
     enable  => true,
     require => Package['avahi', 'nss-mdns'],
   }
+
+  augeas { 'avahi/mdns-host-resolution':
+    context => '/files/etc/nsswitch.conf',
+    changes => [
+      'ins service before database[.="hosts"]/service[.="resolve"]',
+      'set database[.="hosts"]/service[.=""] mdns_minimal',
+      'ins reaction after database[.="hosts"]/service[.="mdns_minimal"]',
+      'set database[.="hosts"]/reaction[preceding-sibling::service[1][.="mdns_minimal"]/status NOTFOUND',
+      'set database[.="hosts"]/reaction[preceding-sibling::service[1][.="mdns_minimal"]/status/action return',
+    ],
+    onlyif  => 'match database[.="hosts"]/service[.="mdns_minimal"] size == 0',
+    lens    => 'Nsswitch.lns',
+    incl    => '/etc/nsswitch.conf',
+    require => Service['avahi-daemon'],
+  }
 }
