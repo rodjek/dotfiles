1f47608b95ed8838a339230a94dfd9a0f248df10
Tim Sharpe <tim@sharpe.id.au>
2018-02-11 11:11:01 +1100

    avahi pt3

--- puppet/modules/avahi/manifests/init.pp
+++ puppet/modules/avahi/manifests/init.pp
@@ -11,10 +11,10 @@ class avahi {
     context => '/files/etc/nsswitch.conf',
     changes => [
       'ins service before database[.="hosts"]/service[.="resolve"]',
-      'set database[.="hosts"]/service[.=""] mdns_minimal',
+      'set database[.="hosts"]/service[.=""] "mdns_minimal"',
       'ins reaction after database[.="hosts"]/service[.="mdns_minimal"]',
-      'set database[.="hosts"]/reaction[preceding-sibling::service[1][.="mdns_minimal"]/status NOTFOUND',
-      'set database[.="hosts"]/reaction[preceding-sibling::service[1][.="mdns_minimal"]/status/action return',
+      'set database[.="hosts"]/reaction[preceding-sibling::service[1][.="mdns_minimal"]/status "NOTFOUND"',
+      'set database[.="hosts"]/reaction[preceding-sibling::service[1][.="mdns_minimal"]/status/action "return"',
     ],
     onlyif  => 'match database[.="hosts"]/service[.="mdns_minimal"] size == 0',
     lens    => 'Nsswitch.lns',
