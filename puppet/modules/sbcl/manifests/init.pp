d03556461a54c9bb3b20b1af9e5d861db56ec995
Tim Sharpe <tim@sharpe.id.au>
2018-02-12 09:20:09 +1100

    sbcl pt2

--- puppet/modules/sbcl/manifests/init.pp
+++ puppet/modules/sbcl/manifests/init.pp
@@ -3,15 +3,36 @@ class sbcl(
 ) {
   package { 'sbcl': }
 
-  arch::aur { ['cl-launch', 'quicklisp']:
+  arch::aur { 'cl-launch']:
     require => Package['sbcl'],
   }
 
+  exec { 'sbcl/quicklisp/download':
+    command => 'curl -O https://beta.quicklisp.org/quicklisp.lisp',
+    path    => ['/usr/bin'],
+    cwd     => '/tmp',
+    user    => $user,
+    group   => $user,
+    creates => "/home/${user}/quicklisp/setup.lisp",
+    notify  => Exec['sbcl/quicklisp/install'],
+  }
+
+  exec { 'sbcl/quicklisp/install':
+    command     => 'sbcl --load /tmp/quicklisp.lisp --eval "(quicklisp-quickstart:install)"',
+    path        => ['/usr/bin'],
+    cwd         => "/home/${user}",
+    user        => $user,
+    group       => $group,
+    refreshonly => true,
+    require     => Package['sbcl'],
+  }
+
   file { "/home/${user}/.sbclrc":
-    ensure => file,
-    owner  => $user,
-    group  => $user,
-    mode   => '0644',
-    source => 'puppet:///modules/dotfiles/sbclrc',
+    ensure  => file,
+    owner   => $user,
+    group   => $user,
+    mode    => '0644',
+    source  => 'puppet:///modules/dotfiles/sbclrc',
+    require => Exec['sbcl/quicklisp/install'],
   }
 }
