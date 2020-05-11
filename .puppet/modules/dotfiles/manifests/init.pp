51ca6b2ac500479717b6681c9a598cbf4054a21d
Tim Sharpe <tim@bombasticmonkey.com>
2020-05-11 17:38:07 +1000

    Remove old dotfile-config.yaml

--- .puppet/modules/dotfiles/manifests/init.pp
+++ .puppet/modules/dotfiles/manifests/init.pp
@@ -1,14 +1,11 @@
 class dotfiles(
-  Array[String[1]] $packages = [],
-  String[1] $base_dir = $facts['dotfiles_base_dir'],
+  String[1]                  $base_dir = $facts['dotfiles_base_dir'],
+  Array[String[1]]           $packages = [],
+  Hash[String[1], Variant[String[1], Undef, Hash]] $symlinks = {},
 ) {
-  $config = dotfiles::load_yaml(dotfiles::filesystem_join($base_dir, 'dotfile-config.yaml'))
-
-  $config.dig('dotfiles', 'symlink').then |$symlinks| {
-    $symlinks.each |$symlink| {
-      dotfiles::symlink { $symlink[0]:
-        params => $symlink[1],
-      }
+  $symlinks.each |$symlink| {
+    dotfiles::symlink { $symlink[0]:
+      params => $symlink[1],
     }
   }
 
