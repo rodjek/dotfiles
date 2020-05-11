886c2f127d4e36155ee66baeed0ac335763d108f
Tim Sharpe <tim@bombasticmonkey.com>
2020-05-11 15:43:15 +1000

    Avoid stty usage in packer

--- .puppet/modules/dotfiles/lib/puppet/provider/package/packer.rb
+++ .puppet/modules/dotfiles/lib/puppet/provider/package/packer.rb
@@ -2,7 +2,11 @@ require 'puppet/provider/package'
 require 'puppet/provider/package/pacman'
 
 Puppet::Type.type(:package).provide(:packer, :parent => :pacman, :source => :pacman) do
-  commands :pacman => File.join(Facter.value(:dotfiles_homedir), '.bin', 'packer')
+  commands :packer => File.join(Facter.value(:dotfiles_homedir), '.bin', 'packer')
 
   defaultfor :operatingsystem => :archlinux
+
+  def pacman(*args)
+    packer(*args)
+  end
 end
