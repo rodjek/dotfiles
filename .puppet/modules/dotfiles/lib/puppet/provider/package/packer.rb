cbefa85c383fb11da90169fe21b801b84845b694
Tim Sharpe <tim@bombasticmonkey.com>
2020-05-11 17:28:07 +1000

    Support virtual packages in packer provider

--- .puppet/modules/dotfiles/lib/puppet/provider/package/packer.rb
+++ .puppet/modules/dotfiles/lib/puppet/provider/package/packer.rb
@@ -2,9 +2,18 @@ require 'puppet/provider/package'
 require 'puppet/provider/package/pacman'
 
 Puppet::Type.type(:package).provide(:packer, :parent => :pacman, :source => :pacman) do
+  desc <<~DESC
+    Override the upstream pacman provider to use the packer wrapper script that
+    adds transparent support for AUR packages.
+  DESC
+
   commands :packer => File.join(Facter.value(:dotfiles_homedir), '.bin', 'packer')
 
   defaultfor :operatingsystem => :archlinux
+  has_feature :install_options
+  has_feature :uninstall_options
+  has_feature :upgradeable
+  has_feature :virtual_packages
 
   def pacman(*args)
     packer(*args)
